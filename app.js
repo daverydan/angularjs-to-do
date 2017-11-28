eb19ac0050f992cacf9a20dd6fbc2c03a258061d
Danny Avery <danny.w.avery@gmail.com>
2017-11-27 19:27:34 -0700

    Create the task list

---
+++ app.js
@@ -0,0 +1,7 @@
+angular
+  .module('to-do', [])
+  .controller('ToDoController', ToDoController);
+
+function ToDoController() {
+  this.tasks = ['Walk the cat', 'Eat second breakfast', 'Watch Rocky cutscenes on youtube since too tired to workout'];
+}
